677e9743ff9e94d09103db0b40c564d881b17b0a
Retr0981 <davidarmah991@gmail.com>
2025-12-04 12:02:26 +0000

    updating file

--- config.js
+++ config.js
@@ -1,53 +1,51 @@
 // Enhanced field name aliases for matching
+// REMOVED generic terms like 'name' and 'fullName' that caused false matches
 const FIELD_ALIASES = {
   // Personal Information
   firstName: [
     'firstName', 'first_name', 'firstname', 'fname', 'givenName', 'given_name',
-    'forename', 'name', 'fullName', 'user.firstName', 'customer.firstName',
-    'applicant.firstName', 'candidate.firstName', 'first', 'fn', 'given',
-    'firstName', 'fName', 'firstName1', 'firstname1', 'name_first'
+    'forename', 'user.firstName', 'customer.firstName', 'applicant.firstName',
+    'candidate.firstName', 'first', 'fn', 'given', 'fName', 'firstName1',
+    'firstname1', 'name_first'
   ],
   
   lastName: [
     'lastName', 'last_name', 'lastname', 'lname', 'surname', 'familyName',
     'family_name', 'user.lastName', 'customer.lastName', 'applicant.lastName',
-    'candidate.lastName', 'last', 'ln', 'family', 'surname', 'lastName',
-    'lName', 'lastname1', 'name_last'
+    'candidate.lastName', 'last', 'ln', 'family', 'lName', 'lastname1', 'name_last'
   ],
   
   fullName: [
-    'fullName', 'full_name', 'fullname', 'name', 'completeName', 'user.name',
-    'customer.name', 'applicant.name', 'candidate.name', 'person.name',
-    'displayName', 'display_name'
+    'fullName', 'full_name', 'fullname', 'completeName', 'displayName',
+    'user.name', 'customer.name', 'applicant.name', 'candidate.name',
+    'person.name', 'display_name'
   ],
   
   email: [
     'email', 'e-mail', 'emailAddress', 'email_address', 'e_mail', 'mail',
-    'e mail', 'emailaddress', 'contact', 'contactEmail', 'candidate.email',
-    'applicant.email', 'user.email', 'person.email', 'contact_email',
-    'emailAddr', 'mailAddress', 'email_address', 'e-mailAddress', 'email_addr'
+    'e mail', 'emailaddress', 'contactEmail', 'candidate.email', 'applicant.email',
+    'user.email', 'person.email', 'contact_email', 'emailAddr', 'mailAddress',
+    'e-mailAddress', 'email_addr'
   ],
   
   phone: [
     'phone', 'phoneNumber', 'phone_number', 'telephone', 'mobile', 'cell',
-    'cellphone', 'phonenumber', 'tel', 'contact', 'contactNumber',
-    'candidate.phone', 'applicant.phone', 'user.phone', 'person.phone',
-    'contact_phone', 'phone_no', 'telephone_no', 'mobileNumber', 'phoneNumber1',
-    'telephoneNumber', 'mobilePhone', 'cellPhone'
+    'cellphone', 'phonenumber', 'tel', 'contactNumber', 'candidate.phone',
+    'applicant.phone', 'user.phone', 'person.phone', 'contact_phone', 'phone_no',
+    'telephone_no', 'mobileNumber', 'phoneNumber1', 'telephoneNumber', 'mobilePhone'
   ],
   
   // Address Information
   address: [
     'address', 'streetAddress', 'street_address', 'addressLine1', 'address1',
-    'line1', 'street', 'location', 'mailingAddress', 'residentialAddress',
-    'homeAddress', 'workAddress', 'address_line1', 'addr1', 'streetAddr',
-    'streetaddress', 'addrLine1', 'street_address1'
+    'line1', 'street', 'mailingAddress', 'residentialAddress', 'homeAddress',
+    'workAddress', 'address_line1', 'addr1', 'streetAddr', 'addrLine1'
   ],
   
   city: [
     'city', 'town', 'cityName', 'locality', 'addressCity', 'homeCity',
     'workCity', 'city_name', 'locationCity', 'address_city', 'cityTown',
-    'city_town', 'address_city', 'locality_city'
+    'city_town', 'locality_city'
   ],
   
   state: [
@@ -64,15 +62,14 @@ const FIELD_ALIASES = {
   
   country: [
     'country', 'countryName', 'nation', 'addressCountry', 'homeCountry',
-    'workCountry', 'country_name', 'nationality', 'country_nation',
-    'address_country'
+    'workCountry', 'country_name', 'nationality', 'address_country'
   ],
   
   // Professional Information
   company: [
     'company', 'organization', 'employer', 'companyName', 'company_name',
     'organizationName', 'currentCompany', 'employerName', 'companyName1',
-    'compName', 'orgName', 'employer_name', 'current_employer', 'currentCompany'
+    'compName', 'orgName', 'employer_name', 'current_employer'
   ],
   
   jobTitle: [
@@ -84,7 +81,7 @@ const FIELD_ALIASES = {
   // Additional Fields
   website: [
     'website', 'personalWebsite', 'portfolio', 'url', 'websiteUrl',
-    'webSite', 'site', 'personal_site', 'portfolio_url', 'website_url'
+    'webSite', 'personal_site', 'portfolio_url', 'website_url'
   ],
   
   linkedin: [
@@ -125,40 +122,4 @@ const FIELD_ALIASES = {
     'noticePeriod', 'notice', 'availability', 'whenAvailable',
     'notice_period', 'availability_date', 'start_date', 'joining_date'
   ]
-};
-
-// Field type patterns for better matching
-const FIELD_PATTERNS = {
-  email: /email|e.?mail|mail.?address/i,
-  phone: /phone|mobile|tel|cell|contact.?number/i,
-  date: /date|dob|birth.?date|birthday/i,
-  url: /url|website|web.?site|link/i,
-  file: /file|upload|attachment|cv|resume|document/i
-};
-
-// Common checkbox patterns that should be auto-checked
-const AUTO_CHECK_PATTERNS = [
-  /terms|conditions|agreement|privacy|policy|consent|accept|agree|acknowledge/i,
-  /newsletter|subscription|updates|notifications|marketing/i,
-  /opt.?in|subscribe|sign.?up/i
-];
-
-// Common dropdown values for auto-selection
-const COMMON_VALUES = {
-  country: {
-    'united states': ['us', 'usa', 'united states', 'america'],
-    'united kingdom': ['uk', 'gb', 'great britain', 'england'],
-    'canada': ['ca', 'canada'],
-    'australia': ['au', 'australia']
-  },
-  gender: {
-    'male': ['male', 'm', 'man', 'gentleman'],
-    'female': ['female', 'f', 'woman', 'lady'],
-    'other': ['other', 'prefer not to say', 'non-binary']
-  },
-  employment: {
-    'full-time': ['full.time', 'fulltime', 'permanent'],
-    'part-time': ['part.time', 'parttime'],
-    'contract': ['contract', 'freelance', 'consultant']
-  }
 };
